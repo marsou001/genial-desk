55ac24d5480dbf6732f110db74178b2f1aa9aa20
marsou001 <61951643+marsou001@users.noreply.github.com>
2026-02-25 15:23:58 +0000

    stay on same page on org switch

--- components/Layout/OrganizationSwitcher.tsx
+++ components/Layout/OrganizationSwitcher.tsx
@@ -3,10 +3,13 @@
 import { useState, useRef, useEffect } from "react";
 import { OrganizationView as Organization } from "@/types";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 
 export default function OrganizationSwitcher({ organizations }: { organizations: Organization[] }) {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname()
+  const page = pathname.split("/")[3]
+  
   const containerRef = useRef<HTMLDivElement>(null);
   
   const { id: currentOrganizationId } = useParams()
@@ -34,7 +37,7 @@ export default function OrganizationSwitcher({ organizations }: { organizations:
       <button
         type="button"
         onClick={() => setIsOpen((prev) => !prev)}
-        className="px-4 py-2 rounded-lg border bg-white dark:bg-zinc-900 border-zinc-300 dark:border-zinc-700 text-sm font-medium hover:bg-zinc-100 dark:hover:bg-zinc-800 transition"
+        className="px-4 py-2 rounded-lg cursor-pointer border bg-white dark:bg-zinc-900 border-zinc-300 dark:border-zinc-700 text-sm font-medium hover:bg-zinc-100 dark:hover:bg-zinc-800 transition"
       >
         {currentOrganization ? currentOrganization.name : "All organizations"}
       </button>
@@ -63,7 +66,7 @@ export default function OrganizationSwitcher({ organizations }: { organizations:
             {organizations.map((org) => (
               <li key={org.id} className="w-full">
                 <Link
-                  href={`/organizations/${org.id}/dashboard`}
+                  href={`/organizations/${org.id}/${page}`}
                   className={`inline-block w-full text-left px-4 py-2 hover:bg-zinc-100 dark:hover:bg-zinc-800 ${
                     org.id === currentOrganizationId
                       ? "font-semibold"
