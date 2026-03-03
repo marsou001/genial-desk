8366797c3ed581acacb19fb3286caeb29603ba77
marsou001 <61951643+marsou001@users.noreply.github.com>
2026-03-03 10:48:10 +0000

    add images stored in supabase to next.js remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_APP_HOSTNAME!,
+      }
+    ]
+  }
 };
 
 export default nextConfig;
