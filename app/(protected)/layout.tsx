d234a56d9acc7cca9a8606eae0fb9945ee96c033
marsou001 <61951643+marsou001@users.noreply.github.com>
2026-03-27 18:41:34 +0100

    show email confirmation status via Context API

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,15 @@
+import { EmailConfirmationProvider } from '@/context/email-confirmation-context';
+import { getUser } from '@/lib';
+
+export default async function ProtectedPagesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getUser();
+  const isEmailConfirmed = user.email_confirmed_at !== null;
+
+  return (
+    <EmailConfirmationProvider value={isEmailConfirmed}>{children}</EmailConfirmationProvider>
+  );
+}
