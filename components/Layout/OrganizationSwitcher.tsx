4c15b1b919a6de32a50ff2d10873ca7d0348c0f7
marsou001 <61951643+marsou001@users.noreply.github.com>
2026-04-26 17:48:55 +0100

    use useIsClickedOutside to control outside click for org switcher component

--- components/Layout/OrganizationSwitcher.tsx
+++ components/Layout/OrganizationSwitcher.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useRef, useEffect } from "react";
 import { OrganizationView as Organization } from "@/types";
 import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { useIsClickedOutside } from "@/hooks/useIsClickedOutside";
 
 export default function OrganizationSwitcher({
   organizations,
 }: {
   organizations: Organization[];
 }) {
-  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
@@ -18,27 +17,12 @@ export default function OrganizationSwitcher({
     searchParams.size > 0 ? "?" + searchParams.toString() : "";
   const page = (pathname + queryParams).split("/")[3] ?? "dashboard";
 
-  const containerRef = useRef<HTMLDivElement>(null);
+  const { containerRef, open, setOpen } = useIsClickedOutside();
 
   const { id: currentOrganizationId } = useParams();
   const currentOrganization = currentOrganizationId
     ? organizations.find((org) => org.id === currentOrganizationId)
     : null;
-
-  // Close when clicking outside
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        containerRef.current &&
-        !containerRef.current.contains(event.target as Node)
-      ) {
-        setIsOpen(false);
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
 
   return organizations.length === 0 ? (
     <Link
@@ -52,14 +36,14 @@ export default function OrganizationSwitcher({
       {/* Trigger */}
       <button
         type="button"
-        onClick={() => setIsOpen((prev) => !prev)}
+        onClick={() => setOpen((prev) => !prev)}
         className="px-4 py-2 rounded-lg cursor-pointer border bg-white dark:bg-zinc-900 border-zinc-300 dark:border-zinc-700 text-sm font-medium hover:bg-zinc-100 dark:hover:bg-zinc-800 transition"
       >
         {currentOrganization ? currentOrganization.name : "All organizations"}
       </button>
 
       {/* Dropdown */}
-      {isOpen && (
+      {open && (
         <div className="absolute min-w-40 mt-2 rounded-xl overflow-hidden shadow-lg bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700 z-50">
           <ul className="text-sm">
             {/* All organizations option */}
